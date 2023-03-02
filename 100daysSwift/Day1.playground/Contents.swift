58a9570bc1878e04a961e82487b5549617df304f
Ramazan Amangeldin <ramazanamangeldin@MacBook-Pro-Ramazan.local>
2023-03-02 19:06:33 +0600

    add new folder

---
+++ 100daysSwift/Day1.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var str = "Hello World"
+
+str = "Goodbye"
+
+var age = 38
+
+var population = 8_000_000
+
+var str1 = """
+Ramazan goes
+to football
+matches
+"""
+
+var str2 = """
+Ramazan goes \
+to football \
+matches
+"""
+
+var pi = 3.141
+
+var awesome = true
+var bad = false
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+var results = "The test results are here: \(str3)"
+
+let taylor = "Swift"
+
+let strr = "Hello Playground"
+let album: String = "Weekend"
+let year: Int = 2023
+let height: Double = 18.1
+let taylorRocks: Bool = truer
